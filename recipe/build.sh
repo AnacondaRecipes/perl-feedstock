50107ab842a4efc5fa57a5bdfbfce414f54e1be6
Charles Bousseau <cbousseau@anaconda.com>
2025-05-13 14:48:55 -0400

    update to 5.40.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -56,7 +56,5 @@ make
 # change permissions again after building
 chmod -R o-w "${SRC_DIR}"
 
-# 1/13/2025
-# Still getting several failing tests, see: https://github.com/AnacondaRecipes/perl-feedstock/pull/11#issuecomment-2583033446
-# make test
+make test
 make install
