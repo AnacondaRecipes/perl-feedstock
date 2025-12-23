1a6b4647e8415c293bf0c8a0ebc4f73754ff072b
Eric Lundby <Eric.Lundby@gmail.com>
2025-12-22 17:23:31 -0700

    Revert back to working replacement.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,12 +3,12 @@ cd win32
 :: Set the compiler to use vs2019
 sed -i.bak 's/^#\s*CCTYPE\s*=\s*MSVC142/CCTYPE = MSVC142/' Makefile
 
-:: Override the default install prefix. Backslashes need escaped (and preserved, forward slashes dont work)
-:: escape for sed  (\ -> \\)
-set "SED_LIBPREFIX=%LIBRARY_PREFIX:\=\\%"
-:: escape AGAIN for cmd/MSYS (\\ -> \\\\)
-set "SED_LIBPREFIX=%SED_LIBPREFIX:\=\\%"
-sed -i.bak -E "s|^([[:space:]]*INST_TOP[[:space:]]*=).*|\1 !SED_LIBPREFIX!|" Makefile
+:: Override the default install prefix
+:: Note: backslashes are required, therefore I didn't find a reliable way to use sed.
+powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+  "$p = Get-Content -Raw Makefile; " ^
+  "$p = [regex]::Replace($p, '(^\s*INST_TOP\s*=).*$', '${1} ' + $env:LIBRARY_PREFIX, [Text.RegularExpressions.RegexOptions]::Multiline); " ^
+  "[System.IO.File]::WriteAllText('Makefile', $p, [System.Text.Encoding]::ASCII)"
 
 nmake
 nmake test || echo Ignoring test failure
